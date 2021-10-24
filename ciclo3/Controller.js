7625f3ca9f5da06f99e07d74c604f6707882685f
natalia-oyama <nataliamami37@gmail.com>
2021-10-24 16:52:56 -0300

    organizado

--- ciclo3/Controller.js
+++ ciclo3/Controller.js
@@ -1,22 +1,49 @@
 const express = require('express');
 const cors = require('cors');
+const models = require('./models');
 const app = express();
 
 app.use(cors());
+
+let cliente = models.Cliente;
+let itemPedido = models.ItemPedido;
+let pedido = models.Pedido;
+let servico = models.Servico;
+
 app.get('/', function(req, res){
     res.send('Olá, Mundo!');
 });
 
 app.get('/clientes', function(req, res){
-    res.send('Seja bem-vindo(a) a Services TI.');
+    await cliente.create({
+        nome: "Natalia",
+        endereco: "Rua Lua Nova",
+        cidade: "Maringá",
+        uf: "PR",
+        nascimento: "1999-12-12",
+        clienteDesde: "2015-01-15"
+    });
+
+    res.send('Cliente adicionado com sucesso.');
 });
 
-app.get('/servicos', function(req, res){
-    res.send('Área de serviços.');
+app.get('/servicos', async(req, res)=>{
+    await servico.create({
+        nome: "Nodejs",
+        descricao: "Desenvolvimento de aplicação back-end.",
+        creatAt: new Date(),
+        updateAt: new Date()
+    });
+
+    res.send('Serviço criado com sucesso.');
 });
 
 app.get('/pedidos', function(req, res){
-    res.send('Área de pedidos.');
+    await pedido.creat({
+        dataPedido: "2016-11-10"
+    });
+
+    res.send('Pedido registrado com sucesso.');
 });
 
 let port = process.env.PORT || 3001;
